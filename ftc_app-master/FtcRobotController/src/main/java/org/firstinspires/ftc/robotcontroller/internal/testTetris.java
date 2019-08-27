0c055619cb0fa1a649184e67f8e8557db9f752ce
cgald <chase_gould@northlandchristian.org>
2019-08-27 15:05:00 -0500

    Both tests and revTele

--- ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/testTetris.java
+++ ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/testTetris.java
@@ -1,23 +1,27 @@
 package org.firstinspires.ftc.robotcontroller.internal;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "testRev", group = "9191")
-public class test extends OpMode {
+@TeleOp(name = "testTetris", group = "9191")
+public class testTetris extends OpMode {
     private DcMotor motor1;
     private DcMotor motor2;
     private DcMotor motor3;
-    private DcMotor motor4;
+    private Servo servo1;
     @Override
     public void init() {
         motor1 = hardwareMap.dcMotor.get("m1");
         motor2 = hardwareMap.dcMotor.get("m2");
         motor3 = hardwareMap.dcMotor.get("m3");
-        motor4 = hardwareMap.dcMotor.get("m4");
+        servo1 = hardwareMap.servo.get("s1");
+
         motor2.setDirection(DcMotorSimple.Direction.REVERSE);
     }
+
     @Override
     public void loop() {
         if(gamepad1.left_stick_y != 0){
@@ -26,18 +30,15 @@ public class test extends OpMode {
         } else if(gamepad1.right_stick_x !=0){
             motor1.setPower(gamepad1.right_stick_x * -.5);
             motor2.setPower(gamepad1.right_stick_x * .5);
-        } else{
+        } else {
             motor1.setPower(0);
             motor2.setPower(0);
         } if(gamepad2.left_stick_y != 0){
             motor3.setPower(gamepad2.left_stick_y);
-        } else{
-            motor3.setPower(0);
-        } if(gamepad2.right_stick_x != 0){
-            motor4.setPower(gamepad2.right_stick_x * .5);
-        } else{
-            motor4.setPower(0);
+        } if(gamepad2.a){
+            servo1.setPosition(servo1.getPosition() + .01);
+        } else if(gamepad2.y){
+            servo1.setPosition(servo1.getPosition() - .01);
         }
-        telemetry.addData("#", gamepad1.left_stick_y);
     }
 }
